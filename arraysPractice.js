b38a48842aa2e5599554e43ac08bf8f527dd7965
Nick Hathaway <Nicholashathaway@gmail.com>
2017-09-26 15:40:53 -0600

    define first, last, looper

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,7 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
-  //Code Here
+  var first = (arr) => arr[0];
 
 
 //Next problem
@@ -15,7 +15,7 @@ var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
 
 
-  //Code Here
+  var last = (arr) => arr[arr.length - 1];
 
 
 //Next Problem
@@ -24,7 +24,7 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+  var looper = (family) => family.map((member) => alert(member));
 
 
 //Next problem
